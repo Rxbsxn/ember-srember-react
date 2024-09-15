0ed666611bc9b22a5f924c74b498b07e287bd8af
Robert Harężlak <harezlak.robert7@gmail.com>
2024-09-15 10:53:02 +0200

    asdasda

--- src/MyReactComponent.tsx
+++ src/MyReactComponent.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // src/MyReactComponent.jsx
 // @ts-nocheck
-import React from 'react';
+import React, { useState } from 'react';
 
 const MyReactComponent = (props: any) => {
+	const [count, setCount] = useState(0)
   const { message, onEvent } = props;
 
   const handleClick = () => {
@@ -12,10 +13,17 @@ const MyReactComponent = (props: any) => {
     }
   };
 
+	const updateCounterFromReact = () => {
+		setCount((prevValue) => prevValue + 1)
+	}
+
   return (
     <div>
-      <h2>React Application</h2>
+      <h2>React Application Embedded via Script</h2>
       <p>{message}</p>
+			<p>{count}</p>
+
+			<button onClick={updateCounterFromReact}>Update Counter</button>
       <button onClick={handleClick}>Send Data to Ember</button>
     </div>
   );
